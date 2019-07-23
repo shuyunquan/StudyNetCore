10e91c9753bed3fcbaab330ce2286d93680c01ab
JIKIUJ <shuyunquan@qq.com>
2019-07-24 00:15:54 +0800

    add HTTPS

--- StudyNetCore/Startup.cs
+++ StudyNetCore/Startup.cs
@@ -12,9 +12,11 @@ namespace StudyNetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IStudentService,StudentService>();
-
             services.AddMvc();
-
+            services.AddHttpsRedirection(option=> {
+                option.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                option.HttpsPort = 5001;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILogger<Startup> logger)
@@ -23,7 +25,7 @@ namespace StudyNetCore
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseHttpsRedirection();
             app.UseMvc(route =>
             {
                 route.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
