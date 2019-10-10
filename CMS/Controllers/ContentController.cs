b13666906a6edd23aabe6705b3864d134320eee8
JIKIUJ <shuyunquan@qq.com>
2019-10-10 09:37:11 +0800

    addCMS

---
+++ CMS/Controllers/ContentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace CMS.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> options)
+        {
+            contents = options.Value;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new List<Content> { contents });
+        }
+    }
+}
