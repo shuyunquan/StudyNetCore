700f623677bad1d2e5036e86d953d4413f4c770c
JIKIUJ <shuyunquan@qq.com>
2019-11-03 17:34:43 +0800

    cao

---
+++ WebAPI/AuthHelper/JWTAuth.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.AuthHelper
+{
+    public static class JWTAuth
+    {
+        public static string CreateToken(Dictionary<string, object> payLoad, int expiresMinute, Dictionary<string, object> header = null)
+        {
+            if (header == null)
+            {
+                header = new Dictionary<string, object>(new List<KeyValuePair<string, object>>() {
+                    new KeyValuePair<string, object>("alg", "HS256"),
+                    new KeyValuePair<string, object>("typ", "JWT")
+                });
+            }
+            //添加jwt可用时间（应该必须要的）
+            var now = DateTime.UtcNow;
+            payLoad["nbf"] = ToUnixEpochDate(now);//可用时间起始
+            payLoad["exp"] = ToUnixEpochDate(now.Add(TimeSpan.FromMinutes(expiresMinute)));//可用时间结束
+
+            var encodedHeader = Base64UrlEncoder.Encode(JsonConvert.SerializeObject(header));
+            var encodedPayload = Base64UrlEncoder.Encode(JsonConvert.SerializeObject(payLoad));
+
+            var hs256 = new HMACSHA256(Encoding.ASCII.GetBytes("Audience:Secret"));
+            var encodedSignature = Base64UrlEncoder.Encode(hs256.ComputeHash(Encoding.UTF8.GetBytes(string.Concat(encodedHeader, ".", encodedPayload))));
+
+            var encodedJwt = string.Concat(encodedHeader, ".", encodedPayload, ".", encodedSignature);
+            return encodedJwt;
+        }
+        public static long ToUnixEpochDate(DateTime date) =>
+        (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+    }
+}
--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebAPI.Controllers
 {
@@ -35,5 +36,31 @@ namespace WebAPI.Controllers
             })
             .ToArray();
         }
+
+        //iss(issuer)：签发人
+        //exp(expiration time)：过期时间
+        //sub(subject)：主题
+        //aud(audience)：受众
+        //nbf(Not Before)：生效时间
+        //iat(Issued At)：签发时间
+        //jti(JWT ID)：编号
+        public void TokenValidateTest()
+        {
+            Dictionary<string, object> payLoad = new Dictionary<string, object>();
+            payLoad.Add("sub", "JWT主题");
+            payLoad.Add("jti", "09e572c7-62d0-4198-9cce-0915d7493806");
+            payLoad.Add("nbf", null);
+            payLoad.Add("exp", null);
+            payLoad.Add("iss", "Vae");
+            payLoad.Add("aud", "shuyunquan");
+            payLoad.Add("age", 30);
+            payLoad.Add("name", "许嵩");
+
+            var encodeJwt = TokenContext.CreateToken(payLoad, 30);
+
+            var result = TokenContext.Validate(encodeJwt, (load) => { return true; });
+
+        }
+
     }
 }
