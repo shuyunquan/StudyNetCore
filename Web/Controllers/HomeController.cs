d00921adbc8aae74c3bfe2b85bc0da9f1726cf74
JIKIUJ <shuyunquan@qq.com>
2019-11-25 23:47:43 +0800

    SqlSugar

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using DomainModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SqlSugar;
 using Web.Models;
 
 namespace Web.Controllers
@@ -35,5 +37,49 @@ namespace Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public void SqlSugarTest()
+        {
+            SqlSugarClient db = new SqlSugarClient(
+            new ConnectionConfig()
+            {
+                ConnectionString = "server=.;uid=sa;pwd=test123;database=VaeDB",
+                DbType = DbType.SqlServer,//设置数据库类型
+                IsAutoCloseConnection = true,//自动释放数据务，如果存在事务，在事务结束后释放
+                //InitKeyType = InitKeyType.Attribute //从实体特性中读取主键自增列信息
+            });
+
+
+            //用来打印Sql方便你调式    
+            db.Aop.OnLogExecuting = (sql, pars) =>
+            {
+                Console.WriteLine(sql + "\r\n" +
+                db.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName, it => it.Value)));
+                Console.WriteLine();
+            };
+
+
+            /*查询*/
+            var list = db.Queryable<Movie>().ToList();//查询所有
+            var getByWhere = db.Queryable<Movie>().Where(it => it.ID == 1).ToList();//根据条件查询
+            var total = 0;
+            var getPage = db.Queryable<Movie>().Where(it => it.ID == 1).ToPageList(1, 2, ref total);//根据分页查询
+                                                                                                           //多表查询用法 http://www.codeisbug.com/Doc/8/1124
+
+            /*插入*/
+            var data = new Movie() { Title = "诸葛亮",ReleaseDate= DateTime.Parse("2019-09-09"),Genre="丞相" };
+            db.Insertable(data).ExecuteReturnIdentity();
+            //更多插入用法 http://www.codeisbug.com/Doc/8/1130
+
+            /*更新*/
+            var data2 = new Movie() { ID = 1, Genre = "音乐家" };
+            db.Updateable(data2).ExecuteCommand();
+            //更多更新用法 http://www.codeisbug.com/Doc/8/1129
+
+            /*删除*/
+            db.Deleteable<Movie>(3).ExecuteCommand();
+            //更多删除用法 http://www.codeisbug.com/Doc/8/1128
+        }
+
     }
 }
---
+++ Web/run.cmd
@@ -0,0 +1,5 @@
+﻿@echo off
+echo ASPNETCORE_ENVIRONMENT=Development
+dotnet build
+start "Web" dotnet watch run
+exit
