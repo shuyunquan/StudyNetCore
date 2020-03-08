c874fc96085a0dcf0c6d323298901f5aed868c98
JIKIUJ <shuyunquan@qq.com>
2020-03-08 16:44:16 +0800

    add

--- StudyCSharp/Program.cs
+++ StudyCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace StudyCSharp
 {
@@ -9,7 +10,28 @@ namespace StudyCSharp
             var s = ("许嵩", "阿萨", "方式");
             Console.WriteLine(s.Item1);
 
+            int salary = 5000;
+            jiangJin(ref salary);
+            Console.WriteLine(salary);
+
+            int a = 1;
+            int b;
+            int asd = Calcu(a,out b);
+            Console.WriteLine(asd + " : " + b);
+
             Console.Read();
         }
+
+        static void jiangJin(ref int salary)
+        {
+            salary += 500;
+        }
+
+        static int Calcu(int a, out int b)
+        {
+            b = a;
+            return a + b;
+        }
+
     }
 }
